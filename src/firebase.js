b3552a7aaeb11a673785ed1022eaf08cfef1bff2
Aria <appliedalpha@naver.com>
2020-12-14 21:06:06 +0900

    1-2

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCdRtKIrmRoqIR0ePf94kLrXmy3ojMh8-Q",
-    authDomain: "nwitter-67ce0.firebaseapp.com",
-    projectId: "nwitter-67ce0",
-    storageBucket: "nwitter-67ce0.appspot.com",
-    messagingSenderId: "232331614331",
-    appId: "1:232331614331:web:cefe001cd96df4f2fb2ade"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase

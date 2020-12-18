7ca14d4b5cac4835bf3fb07196f201f8ffed3f15
Aria <appliedalpha@naver.com>
2020-12-18 16:14:22 +0900

    2.6

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,18 @@
 import React from "react"
+import {useHistory} from "react-router-dom"
+import {authService} from "fBase";
 
-export default () => <span>Profile</span>;
+export default () => {
+    const history = useHistory();
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/");
+    };
+
+    return (
+        <>
+            <span>Profile</span>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    );
+}

39586a53b2efaeba5cd48464512cdc8d2e472e0d
Aria <appliedalpha@naver.com>
2020-12-20 23:49:31 +0900

    3.0

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,22 @@
-import React from "react"
+import React, {useState} from "react"
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [nweet, setNweet] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+    const onChange = (event) => {
+        const {target: {value}, } = event;
+        setNweet(value);
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="text" value={nweet} onChange={onChange} placeholder="무슨 일이 일어나고 있나요?" maxLength={140} />
+                <input type="submit" value="Nweet" />
+            </form>
+        </div>
+    );
+};
 export default Home;

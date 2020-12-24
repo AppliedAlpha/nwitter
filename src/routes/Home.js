7837c1d258cec56f9f07f66748ba25093bc7626b
Aria <appliedalpha@naver.com>
2020-12-25 02:31:01 +0900

    3.2

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,25 @@
-import React, {useState} from "react"
-import {dbService} from "../fBase";
+import React, {useState, useEffect} from "react"
+import {dbService} from "fBase";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach((document) => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            };
+            setNweets((prevState) => [nweetObject, ...prevState]);
+        });
+    };
+
+    useEffect(() => {
+        getNweets();
+    }, []);
+
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("nweets").add({
@@ -11,6 +28,7 @@ const Home = () => {
         });
         setNweet("");
     };
+
     const onChange = (event) => {
         const {target: {value}, } = event;
         setNweet(value);
@@ -19,9 +37,21 @@ const Home = () => {
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input type="text" value={nweet} onChange={onChange} placeholder="What's Happening?" maxLength={140} />
+                <input type="text"
+                       value={nweet}
+                       onChange={onChange}
+                       placeholder="What's Happening?"
+                       maxLength={140}
+                />
                 <input type="submit" value="Nweet" />
             </form>
+            <div>
+                {nweets.map(nweet =>
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };

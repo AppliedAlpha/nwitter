c6a75ba264033331e9c6d7a2a1a9acf17f02b5e4
Aria <appliedalpha@naver.com>
2020-12-26 02:38:10 +0900

    4.1

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import Nweet from "components/Nweet";
 const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState();
 
     useEffect(() => {
         dbService.collection("nweets").onSnapshot((snapshot) => {
@@ -33,6 +34,21 @@ const Home = ({ userObj }) => {
         setNweet(value);
     }
 
+    const onFileChange = (event) => {
+        const {target: {files}, } = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {currentTarget: {result}, } = finishedEvent;
+            setAttachment(result);
+        };
+        reader.readAsDataURL(theFile);
+    };
+
+    const onClearAttachment = () => {
+        setAttachment(null);
+    };
+
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -42,7 +58,16 @@ const Home = ({ userObj }) => {
                        placeholder="What's Happening?"
                        maxLength={140}
                 />
+                <input type="file"
+                       accept="image/*"
+                       onChange={onFileChange} />
                 <input type="submit" value="Nweet" />
+                {attachment && (
+                    <div>
+                        <img src={attachment} width="50px" height="50px" />
+                        <button onClick={onClearAttachment}>Clear Image</button>
+                    </div>
+                )}
             </form>
             <div>
                 {nweets.map(nweet =>

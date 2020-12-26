d2eb403ea684f2e246b5bd41d8a167519cc383ef
Aria <appliedalpha@naver.com>
2020-12-26 22:27:56 +0900

    5.0

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,26 @@
-import React from "react"
+import React, {useEffect} from "react"
 import {useHistory} from "react-router-dom"
-import {authService} from "fBase";
+import {authService, dbService} from "fBase";
 
-export default () => {
+export default ({ userObj }) => {
     const history = useHistory();
+
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
     };
+
+    const getMyNweets = async () => {
+        const nweets = await dbService.collection("nweets")
+            .where("creatorId", "==", userObj.uid)
+            .orderBy("createdAt", "desc")
+            .get();
+        console.log(nweets.docs.map((doc) => doc.data()));
+    };
+
+    useEffect(() => {
+        getMyNweets();
+    }, []);
 
     return (
         <>
